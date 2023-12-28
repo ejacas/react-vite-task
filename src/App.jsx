8738b068144300d2f93947f866b3ed8e693c5f69
ejacas <evaldasjacas.ej@gmail.com>
2023-12-28 15:43:22 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+
+  return (
+    <>
+     <Header/>
+     <main>
+      <h1>Portfolio</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe eos repellat quam ipsam possimus nisi asperiores obcaecati. Amet, natus aliquid facilis est distinctio, id quisquam perspiciatis molestias accusamus vitae magni.</p>
+     </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css'
+
+function Header() {
+
+  return (
+    <>
+     <header>
+      <div className='background-image'></div>
+      <h2>Title</h2>
+      <p>Some about text in two lines</p>
+      <button>Button</button>
+     </header>
+    </>
+  )
+}
+
+export default Header
